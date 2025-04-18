880b8ff643cd30156456653151d50a317c674ae0
Dark-GreenCat <tranquanghuy3008@outlook.com.vn>
2025-04-18 18:22:47 +0700

    [MODIFY] Modify the size of BRAM

--- microblaze_test.srcs/sources_1/imports/hdl/system_wrapper.v
+++ microblaze_test.srcs/sources_1/imports/hdl/system_wrapper.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2024.1 (lin64) Build 5076996 Wed May 22 18:36:09 MDT 2024
-//Date        : Fri Apr 18 14:34:51 2025
+//Date        : Fri Apr 18 17:19:15 2025
 //Host        : edabk running 64-bit CentOS Linux release 7.9.2009 (Core)
 //Command     : generate_target system_wrapper.bd
 //Design      : system_wrapper
